c518c0c13eb4c8c9ff88320ccc5c74cac29d5c6e
MarcChedefaux <chedefaux.marc@gmail.com>
2023-09-16 17:18:52 +0200

    Added documentation

--- scraping.py
+++ scraping.py
@@ -7,6 +7,15 @@ import json
 url = "https://www.cngsante.fr/chiron/celine/finalnormcesp.html"
 
 def getSpecialities(tableSoup : BeautifulSoup) -> (np.ndarray, np.ndarray):
+    """Extract the different specialities from the webpage
+
+    Args:
+        tableSoup (BeautifulSoup): Soup of the table in the webpage
+
+    Returns:
+        np.ndarray: Specialities Id
+        np.ndarray: Specialities Complete Name
+    """
     specialitesId = []
     specialitesTotal = []
 
@@ -29,6 +38,14 @@ def getSpecialities(tableSoup : BeautifulSoup) -> (np.ndarray, np.ndarray):
     return specialitesId[np.argsort(indId)], specialitesTotal[np.argsort(indTotal)]
 
 def getCities(tableSoup : BeautifulSoup) -> np.ndarray:
+    """Extract the different cities from the webpage
+
+    Args:
+        tableSoup (BeautifulSoup): Soup of the table in the webpage
+
+    Returns:
+        np.ndarray: Cities Names
+    """
     cities = []
     cit = tableSoup.find_all("td", class_="rg")
     for c in cit :
@@ -42,7 +59,17 @@ def getCities(tableSoup : BeautifulSoup) -> np.ndarray:
 
     return cities[np.argsort(ind)]
 
-def getPlaces(tableSoup : BeautifulSoup, specLength : int, citiLength) -> np.ndarray:
+def getPlaces(tableSoup : BeautifulSoup, specLength : int, citiLength : int) -> np.ndarray:
+    """Extract the number of availables places for all specialities in all cities
+
+    Args:
+        tableSoup (BeautifulSoup): Soup of the table in the webpage
+        specLength (int): Number of specialities
+        citiLength (int): Number of cities
+
+    Returns:
+        np.ndarray: Matrix of all the available places
+    """
     places = np.zeros((citiLength, specLength))
 
     placeAvailable = tableSoup.find_all("td", class_="rk")
@@ -54,6 +81,12 @@ def getPlaces(tableSoup : BeautifulSoup, specLength : int, citiLength) -> np.nda
     return places
 
 def saveSpecialitiesJson(specId : np.ndarray, specDesc : np.ndarray) -> None:
+    """Save a json of all specialities with configurable variables
+
+    Args:
+        specId (np.ndarray): Specialities IDs
+        specDesc (np.ndarray): Specialities Complete Names
+    """
     specDict = []
     for i in range(len(specId)):
         sDict = {"Id":specId[i], "Name":specDesc[i], "choosenCoef" : 1, "abilityToIgnoreCity" : 0}
@@ -63,6 +96,11 @@ def saveSpecialitiesJson(specId : np.ndarray, specDesc : np.ndarray) -> None:
         f.write(json.dumps(specDict, ensure_ascii=False, indent=2))
 
 def saveCitiesJson(cities : np.ndarray) -> None :
+    """Save a json of all cities with configurable variables
+
+    Args:
+        cities (np.ndarray): Cities Names
+    """
     citiesDict = []
     for i in range(len(cities)):
         cDict = {"Name":cities[i], "choosenCoef" : 1}
